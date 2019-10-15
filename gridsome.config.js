ba42c19957eb6516dc6a3409193151f2e63e7d82
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2019-10-15 21:54:46 +0300

    First version site MT

---
+++ gridsome.config.js
@@ -0,0 +1,49 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Mafia Travel',
+  siteDescription: 'Цікаві місця для трекінгу а також закинуті, мальовниці місця  України та не тільки для класних мандрівок',
+
+  plugins: [
+    {
+      // Create posts from markdown files
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: 'content/posts/*.md',
+        route: '/:slug',
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: 'Tag',
+            route: '/tag/:id',
+            create: true
+          }
+        }
+      }
+    },
+      {
+      use: '@zefman/gridsome-source-instagram',
+      options: {
+        username: 'vmartuniyk' ,// Instagram username
+        typeName: 'mafia_travel' // The GraphQL type you want the photos to be added under. Defaults to InstagramPhoto
+      }
+    }
+  ],
+
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  },
+}
